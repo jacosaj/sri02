be3ac26e48cc3df4d5604cf15702bedae75964fe
jacosaj <jacekszacek@gmail.com>
2026-03-15 19:37:15 +0100

    inicjalizacja projektu

---
+++ src/test/java/edu/pja/sri/s34669/sn_02/Sn02ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pja.sri.s34669.sn_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sn02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
